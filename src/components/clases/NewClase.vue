5393b2359d6220928e95e71c5d47b190b4398997
Rojo-Web <santifa2004@gmail.com>
2024-05-19 02:58:48 -0500

    crud vue de clases

---
+++ src/components/clases/NewClase.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="container">
+        <div class="col-12 text-center">
+            <div class="alert alert-success" role="alert">
+                <h1 class="mb-0" style="color: green;">Nueva Clase</h1>
+            </div>
+        </div>
+        <p></p>
+        <p></p>
+        <form @submit.prevent="saveClase">
+
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-4" style="margin-right: 435px;">
+                    <div class="mb-3">
+                        <label for="codigo" class="form-label">Id</label>
+                        <div class="input-group">
+                            <div class="input-group-text"><font-awesome-icon icon="tag" /></div>
+                            <input type="text" class="form-control" id="id" aria-describedby="codigoHelp" name="id"
+                                disabled="disabled" v-model='clase.id'>
+                        </div>
+                        <!-- <div id="codigoHelp" class="form-text">Codigo</div> -->
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center align-items-center">
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nombre</label>
+                        <div class="input-group">
+                            <div class="input-group-text"><font-awesome-icon icon="building" /></div>
+                            <input type="text" class="form-control" id="nombre" aria-describedby="codigoHelp"
+                                name="nombre" v-model='clase.nombre'>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Descripcion</label>
+                        <div class="input-group">
+                            <div class="input-group-text"><font-awesome-icon icon="building" /></div>
+                            <input type="text" class="form-control" id="descripcion" aria-describedby="codigoHelp"
+                                name="descripcion" v-model='clase.descripcion'>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Hora de inicio</label>
+                        <div class="input-group">
+                            <div class="input-group-text"><font-awesome-icon icon="building" /></div>
+                            <input type="datetime-local" class="form-control" id="hora_inicio" aria-describedby="codigoHelp"
+                                name="hora_inicio" v-model='clase.hora_inicio'>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="codigo" class="form-label">Duracion</label>
+                        <div class="input-group">
+                            <div class="input-group-text"><font-awesome-icon icon="bank" /></div>
+                            <input type="number" class="form-control" id="duracion" aria-describedby="codigoHelp"
+                                name="duracion" v-model='clase.duracion'>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="codigo" class="form-label">Instalacion </label>
+                        <div class="input-group">
+                            <select class="form-select" v-model="clase.instalacion_id">
+                                <option v-for="instalacion in instalaciones" v-bind:value="instalacion.id">{{ instalacion.nombre
+                                    }}</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="mb-3">
+                        <label for="codigo" class="form-label">Entrenador </label>
+                        <div class="input-group">
+                            <select class="form-select" v-model="clase.entrenador_id">
+                                <option v-for="entrenador in entrenadores" v-bind:value="entrenador.id">{{ entrenador.nombre
+                                    }}</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="btn btn-primary" type="submit">Save</button>
+            <button class="btn btn-secondary mx-2" @click="cancelar">Cancelar</button>
+        </form>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+import Swal from 'sweetalert2'
+export default {
+    name: 'NewClase',
+    data() {
+        return {
+            clase: {
+                id: 0,
+                nombre: '',
+                descripcion: '',
+                instalacion_id: 0,
+                entrenador_id: 0,
+                hora_inicio: '',
+                duracion: 0,
+            }, instalaciones: [], id: '0', entrenadores: [], id: '0'
+        }
+    },
+    methods: {
+        cancelar() {
+            this.$router.push({ name: 'Clases' })
+        },
+        async saveClase() {
+            console.log(`${this.clase.id} ${this.clase.number} ${this.clase.date} ${this.clase.customer_id} ${this.clase.pay_mode_id}`)
+            const res = await axios.post(`http://127.0.0.1:8000/api/clases/`, this.clase)
+            if (res.status == 200) {
+                this.$router.push({ name: 'Clases' })
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Clase has been saved',
+                    showConfirmButton: false,
+                    timer: 2000
+                })
+            }
+        }
+    },
+    mounted() {
+        axios
+            .get(`http://127.0.0.1:8000/api/instalaciones/`)
+            .then(response => (this.instalaciones = response.data.instalaciones));
+        axios
+            .get(`http://127.0.0.1:8000/api/entrenadores/`)
+            .then(response => (this.entrenadores = response.data.entrenadores));
+    },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,43 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import Clases from '../views/clases.vue';
+import EditarClase from '../components/clases/EditarClase.vue';
+import NewClase from '../components/clases/NewClase.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/clases',
+        name: 'Clases',
+        component: Clases
+    },
+    {
+        path: '/editar-clase/:id',
+        name: 'EditarClase',
+        component: EditarClase
+    },
+    {
+        path: '/add-clases/',
+        name: 'NewClase',
+        component: NewClase
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/AboutView.vue')
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
